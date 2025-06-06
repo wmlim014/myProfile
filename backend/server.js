71c4ce92792d541a1f4f1970347c880f0db6db6c
wmlim014 <166711243+wmlim014@users.noreply.github.com>
2025-06-06 19:27:00 +0800

    update connected db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql'
+import { DB_HOST_NAME, DB_PORT, DB_USER, DB_USER_PASSWORD, DB_NAME} from './secret.js'
 
 const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: DB_HOST_NAME,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_USER_PASSWORD,
+    database: DB_NAME
+});
+
+db.connect((err) => {
+    if(err) {
+        console.log('Databalse connection failed: ', err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+})
+
 app.listen(8081, () => {
     console.log('Listening...');
-})
+});
